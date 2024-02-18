120c861360db8ce7a8f9f91bf9471a1061e39237
vincedl-dev <vincelavador.dev@gmail.com>
2024-02-18 12:01:01 +0800

    setting router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home/index.jsx";
+import ErrorPage from "./pages/Error/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/aniFlixz",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div> This Homepage</div>;
+}
+
+export default HomePage;
